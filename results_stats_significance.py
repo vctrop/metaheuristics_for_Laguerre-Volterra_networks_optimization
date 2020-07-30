d3f962679fbc41400b5c62db08d499fe87ba3fe0
Victor O. Costa <victor-o.c@hotmail.com>
2020-07-30 16:39:04 -0300

    Implement statistical significance extrction using Friedman and Nemenyi tests

--- results_stats_significance.py
+++ results_stats_significance.py
@@ -15,32 +15,52 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# Third party
 import numpy as np
-from scipy.stats import wilcoxon
+import scipy.stats
+import scikit_posthocs as sp
+import matplotlib.pyplot as plt
 
-# finite_acor_1k_solutions
-# finite_acor_1k_test_costs
-# finite_acor_1k_times
+x = np.array([np.random.normal(loc=00.0, scale=1.0, size=30),
+              np.random.normal(loc=19.5, scale=1.0, size=30),
+              np.random.normal(loc=20.0, scale=1.0, size=30)])
 
+# print(np.shape(x))
+fr_test = scipy.stats.friedmanchisquare(*x)
+nm_posthoc = sp.posthoc_nemenyi_friedman(x.T)
+
+cmap = ['1', '#fb6a4a', 'mediumseagreen', 'limegreen', 'palegreen']
+heatmap_args = {'cmap':cmap, 'linewidths': 0.5, 'linecolor': '0.3', 'clip_on': False, 'square': True, 'cbar_ax_bbox': [0.80, 0.35, 0.04, 0.3]}
+sp.sign_plot(nm_posthoc, **heatmap_args)
+plt.show()  
+exit()
+
+function_evals = [i * 100 for i in range(1,101)] + [11000 + i * 1000 for i in range(90)]
+function_evals_of_interest = [1e3, 5e3,1e4,5e4,1e5]
+evals_mask = [eval in function_evals_of_interest for eval in function_evals]
+
+#for algorithm in ['sa', 'acfsa', 'pso', 'aiwpso', 'acor', 'baacor']:
 for system_order in ['finite', 'infinite']:
-    print(system_order)
-    for func_evals in ['1k', '5k', '10k', '50k', '100k']:
-        print(func_evals)
+    print(system_order.upper())
+
+    for eval in range(len(function_evals_of_interest)):
+        algorithms_at_fes = []      
+        for algorithm in ['sa', 'pso', 'acor']:
+            print(algorithm)
+            base_filename   = './results/' + algorithm + '_' + system_order
+            test_costs_mat  = np.load(base_filename + '_test_costs.npy')
+            test_costs_of_interest = test_costs_mat[:, evals_mask]
+            costs_fe = test_costs_of_interest[:, eval]
+            
+            algorithms_at_fes.append(list(costs_fe))
         
-        pso_base_filename   = './results/' + system_order + '_' + 'pso'  + '_' + func_evals + '_'
-        acor_base_filename  = './results/' + system_order + '_' + 'acor' + '_' + func_evals + '_'
-        sa_base_filename    = './results/' + system_order + '_' + 'sa'   + '_' + func_evals + '_'
+            #print(str(function_evals_of_interest[eval]) + ':  \t' + str(np.shape(costs_fe)))
+            print(str(function_evals_of_interest[eval]) + ':  \t' + str(np.mean(costs_fe)))
         
-        # Load data
-        pso_test_costs      = np.load(pso_base_filename + 'test_costs.npy')
-        acor_test_costs      = np.load(acor_base_filename + 'test_costs.npy')
-        sa_test_costs      = np.load(sa_base_filename + 'test_costs.npy')
-        
-        
-        if system_order == 'finite' and func_evals == '100k':
-            stat, p = wilcoxon(acor_test_costs, sa_test_costs)
-        else:
-            stat, p = wilcoxon(acor_test_costs, pso_test_costs)
-        
-        print('p-value = %f' % (p))
-        
+        algorithms_at_fes = np.array(algorithms_at_fes)
+        print('\n\n Statistical significance')
+        # print(np.shape(algorithms_at_fes))    
+        print('Friedman p-val = ' + str(scipy.stats.friedmanchisquare(*algorithms_at_fes)[1]))
+        print(sp.posthoc_nemenyi_friedman(algorithms_at_fes.T))
+        print('\n') 
+           
